58d9556b32b5f5c7d25689335fd9de67bf66a8d7
busra <busrahyt42@gmail.com>
2023-05-05 23:50:45 +0300

    Some changes have been updated

--- src/javaAlgorithmQuestion/IQ_10.java
+++ src/javaAlgorithmQuestion/IQ_10.java
@@ -2,24 +2,24 @@ package javaAlgorithmQuestion;
 
 public class IQ_10 {
 
-    /**
+    /** Question 10
      * Given a string, look for a mirror image(backwards) string at both the beginning and end of
      * the given string. In other words, zero or more characters at the very beginning of the given
      * string, and at the very end of the string in reverse order(possibly overlapping).
      * For example, the string "abXYZba" has the mirror end "ab".
 
      * In birefly, look for a mirror image string at both the beginning and end of the given string.
-     mirrorEnds("abXYZba")         --> "ab"
-     mirrorEnds("abca")            --> "a"
-     mirrorEnds("aba")             --> "aba
-     mirrorEnds("abab")            --> ""
-     mirrorEnds("xxx")             --> "xxx"
-     mirrorEnds("xxYxx")           --> "xxYxx"
-     mirrorEnds("Hi and iH")       --> "Hi"
-     mirrorEnds("x")               --> "x"
-     mirrorEnds("")                --> ""
-     mirrorEnds("123and then 321") --> "123"
-     mirrorEnds("band andab")      --> "ba"
+         mirrorEnds("abXYZba")         --> "ab"
+         mirrorEnds("abca")            --> "a"
+         mirrorEnds("aba")             --> "aba
+         mirrorEnds("abab")            --> ""
+         mirrorEnds("xxx")             --> "xxx"
+         mirrorEnds("xxYxx")           --> "xxYxx"
+         mirrorEnds("Hi and iH")       --> "Hi"
+         mirrorEnds("x")               --> "x"
+         mirrorEnds("")                --> ""
+         mirrorEnds("123and then 321") --> "123"
+         mirrorEnds("band andab")      --> "ba"
      */
 
     public static void main(String[] args) {
